621a16d8d7c5e3dee17e5df2b25ffb792728a835
MohamedRaddaoui <mohamedraddaoui05@gmail.com>
2025-04-25 13:53:02 +0100

    Adding project configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core';import { AIAssistViewModule, ChatUIModule } from '@syncfusion/ej2-angular-interactive-chat';
+import { DialogModule, TooltipModule } from '@syncfusion/ej2-angular-popups';
+import { TextBoxModule, TextAreaModule, NumericTextBoxModule, MaskedTextBoxModule, SliderModule, UploaderModule, ColorPickerModule, SignatureModule, RatingModule, OtpInputModule, SmartTextAreaModule, SpeechToTextModule } from '@syncfusion/ej2-angular-inputs';
+import { ButtonModule, CheckBoxModule, RadioButtonModule, SwitchModule, ChipListModule, FabModule, SpeedDialModule, SmartPasteButtonModule } from '@syncfusion/ej2-angular-buttons';
+
+import {
+  ScheduleModule,
+  RecurrenceEditorModule,
+} from '@syncfusion/ej2-angular-schedule';
+
 import { RouterOutlet } from '@angular/router';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [AIAssistViewModule, ChatUIModule, DialogModule, TooltipModule, TextBoxModule, TextAreaModule, NumericTextBoxModule, MaskedTextBoxModule, SliderModule, UploaderModule, ColorPickerModule, SignatureModule, RatingModule, OtpInputModule, SmartTextAreaModule, SpeechToTextModule, ButtonModule, CheckBoxModule, RadioButtonModule, SwitchModule, ChipListModule, FabModule, SpeedDialModule, SmartPasteButtonModule, 
+    ScheduleModule,
+    RecurrenceEditorModule,
+    RouterOutlet,
+    
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'front-project';
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,18 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { enableProdMode } from '@angular/core';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { environment } from './environment/env';
+import { registerLicense } from '@syncfusion/ej2-base';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+registerLicense(
+  'Ngo9BigBOggjHTQxAR8/V1NNaF5cXmpCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdmWXtcc3VSRWBYV01xW0RWYUA='
+);
+
+if (environment.production) {
+  enableProdMode();
+}
+
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
