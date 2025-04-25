0219ce3a5ff7749c58a2d4846d4cd0d8014d3ffd
MohamedRaddaoui <mohamedraddaoui05@gmail.com>
2025-04-25 13:52:14 +0100

    Defining calendar&chatbot routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { CalendarPageComponent } from './calendar-page/calendar-page.component';
+import { AiAssistantComponent } from './ai-assistant/ai-assistant.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'calendar',
+    component: CalendarPageComponent,
+  },
+  {
+    path: 'chatbot',
+    component: AiAssistantComponent,
+  },
+];
