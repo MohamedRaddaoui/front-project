3e155fcadc76043dd1e5e48b5621c9eb050109e7
eyaBrahmi <beya89031@gmail.com>
2025-05-09 23:12:14 +0200

    update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,9 @@ import { AiAssistantComponent } from './ai-assistant/ai-assistant.component';
 import { ProjectComponent } from './project/project.component';
 import { SideBarComponent } from './side-bar/side-bar.component';
 import { AddProjectComponent } from './add-project/add-project.component';
+import { ScrumProjectComponent } from './scrum-project/scrum-project.component';
+import { StandardProjectComponent } from './standard-project/standard-project.component';
+import { DeleteProjectComponent } from './delete-project/delete-project.component';
 
 
 export const routes: Routes = [
@@ -20,12 +23,6 @@ export const routes: Routes = [
   {
     path: '',
     component:ProjectComponent,
-    children: [
-      {
-        path: '',
-        component: SideBarComponent,
-      }
-    ]
 
   },
 
@@ -40,6 +37,29 @@ export const routes: Routes = [
     ]
 
   },
+
+
+  {
+    path: 'scrum/:id',
+    component:ScrumProjectComponent,
+   
+
+  },
+
+  {
+    path: 'standard/:id',
+    component:StandardProjectComponent,
+    
+
+  },
+
+  {
+    path: 'delete/:id',
+    component:DeleteProjectComponent,
+    
+
+  },
+
 
 
 
